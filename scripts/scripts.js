2967125aac2b16576531c693d4c51d54728fd69d
Katy <ihatetoast@gmail.com>
2021-11-14 13:30:37 -0700

    added logic to turn letters to dashes

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,1 +1,8 @@
 console.log("scripts loaded");
+
+const targetWord = "testword";
+// takes in a string, returns array of dashes
+const makeDashes = (str) => {
+  return str.split("").map((ltr) => " - ");
+};
+const testword = makeDashes(targetWord);
