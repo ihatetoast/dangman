74b1581a691083eb75464bcc91799832a5ad8635
jaymar85 <jermark851@gmail.com>
2021-11-13 23:37:25 -0600

    Get word from array in Scripts

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,1 +1,11 @@
 console.log("scripts loaded");
+const WORDS_ARRAY = ['wheel', 'hamburger', 'coffee', 'muffin', 'sandwich'];
+const startBtn = document.querySelector('#start');
+startBtn.addEventListener('click', getWords);
+
+function getWords() {
+    // alert('Katy stinks!');
+    const targetIndex = Math.floor(Math.random() * WORDS_ARRAY.length);
+    const word = WORDS_ARRAY[targetIndex];
+    console.log(word);
+}
