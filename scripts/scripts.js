f0d7e05e9d3b269fda65a324d633db05172374b8
jaymar85 <jermark851@gmail.com>
2021-11-22 00:46:31 -0600

    adding on game feature

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,4 +1,4 @@
-const WORDS_ARRAY = ["wheel", "hamburger", "coffee", "muffin", "sandwich"];
+const WORDS_ARRAY = ["wheel", "zelda", "coffee", "muffin", "pizza"];
 // HTML VARS
 const startBtn = document.querySelector("#start");
 const dashes = document.getElementById("gamedashes");
@@ -11,6 +11,7 @@ const startGame = () => {
   // disable start button
   startBtn.classList.add("disabled");
   makeGameDashes(gameWord);
+  runGameFeature(gameWord);
 };
 startBtn.addEventListener("click", startGame);
 
@@ -28,5 +29,40 @@ const makeGameDashes = (str) => {
     dashStr += '<span data-location="' + idx + '">' + ltr + "</span>";
   });
   dashStr += "</p>";
-  dashes.innerHTML = dashStr;
+  dashes.innerHTML = dashStr; 
 };
+
+const runGameFeature = (word) => {
+  let guess = ''
+  let correct = false
+
+  do {
+    guess = prompt("Guess a letter of the word I'm thinking about?")
+
+    if (guess === null) {
+      alert('The game as been aborted.')
+      return;
+    }
+
+    correct = checkGuess(word, guess)
+  } while(!correct)
+}
+
+const checkGuess = (word, guess) => {
+  let correct = false
+
+  console.log('checkGuess', word)
+  if(word.includes(guess)) {
+    correct = true
+  } else {
+    alert('Off with your head')
+  }
+}
+
+//get a random word - chk
+//break word into single letter strings
+
+//Guess ===
+////true if player gets right letter
+////false if player gets wrong letter
+//Use do while loop 
