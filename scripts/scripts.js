3fa352b9ba43683b56600e3fdf7c5d6e537c52e7
jaymar85 <jermark851@gmail.com>
2021-11-22 18:18:58 -0600

    updated checkGuess function

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -20,6 +20,7 @@ function getWord() {
   const word = WORDS_ARRAY[targetIndex];
   return word;
 }
+
 // makeGameDashes once
 const makeGameDashes = (str) => {
   let dashStr = "<p>";
@@ -40,29 +41,33 @@ const runGameFeature = (word) => {
     guess = prompt("Guess a letter of the word I'm thinking about?")
 
     if (guess === null) {
-      alert('The game as been aborted.')
+      alert('The game has been aborted.')
       return;
     }
 
     correct = checkGuess(word, guess)
   } while(!correct)
+
 }
 
 const checkGuess = (word, guess) => {
   let correct = false
+  // console.log('checkGuess', word)
 
-  console.log('checkGuess', word)
-  if(word.includes(guess)) {
+  if(!word.includes(guess)) {
     correct = true
   } else {
     alert('Off with your head')
   }
+
+  return correct;
 }
 
-//get a random word - chk
+//get a random word
 //break word into single letter strings
 
 //Guess ===
+//put guess in a input 
 ////true if player gets right letter
 ////false if player gets wrong letter
 //Use do while loop 
