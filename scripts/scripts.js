7b565c0c8878bc321db1aa38de999a28aab038a1
jaymar85 <jermark851@gmail.com>
2021-11-26 23:20:03 -0600

    replaced prompt with input field

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,6 +2,7 @@ const WORDS_ARRAY = ["wheel", "zelda", "coffee", "muffin", "pizza"];
 // HTML VARS
 const startBtn = document.querySelector("#start");
 const dashes = document.getElementById("gamedashes");
+const userLetterChoice = document.getElementById("user-letter-choice")
 
 let dashArr;
 let gameWord;
@@ -33,31 +34,36 @@ const makeGameDashes = (arr) => {
   dashArr = arr.map((ltr) => "_");
   console.log(dashArr)
   dashArr.forEach((dash, idx) => {
-    // dashStr += '<span data-location="' + idx + '">' + ltr + "</span>";
     dashStr += `<span data-location="${idx}">${dash}</span>`
   });
   dashStr += "</p>";
   dashes.innerHTML = dashStr; 
 };
 
+// const userLetterInput = (e) => {
+//   let letter = e.target.value
+//   console.log(letter) // assign this value to guess in runGameFunction()
+//   // return letter
+// }
+// userLetterChoice.addEventListener("input", userLetterInput);
+console.log(userLetterChoice.value)
+
 const runGameFeature = (word) => {
   let guess = ''
 
   do {
-    guess = prompt("Guess a letter of the word I'm thinking about?")
-    
-    if (guess === null) {
-      alert('The game has been aborted.')
-      return;
+    guess = userLetterChoice.value
+
+    if (guess >= 1) {
+      checkGuess(word, guess)
     }
-    checkGuess(word, guess)
+    
     confirmGameOver()
   } while(!isGameOver)
 
 }
 
 const checkGuess = (word, guess) => {
-  // console.log('checkGuess', word)
   if(word.includes(guess)) {
     handleCorrectGuess(guess);
     // makeGameDashes(dashArr)
@@ -67,6 +73,7 @@ const checkGuess = (word, guess) => {
     // how will you handle misses? keep track of bad guesses: head, torso,4 limbs = 5. when badguesses === 5, game is over (isGameOver = true or something)
   }
 }
+
 const handleCorrectGuess = (guess) => {
   gameWord.forEach((ltr, idx) => {
     if(ltr === guess) {
@@ -74,23 +81,22 @@ const handleCorrectGuess = (guess) => {
     }
   })
 }
+
 const handleBadGuess = () => {
-  incorrectGuesses++
+  incorrectGuesses 
   console.log(incorrectGuesses)
   //put bad guesses on the DOM
 }
+
 const confirmGameOver = () => {
-  if(incorrectGuesses === 6) {
+  if(incorrectGuesses === 6 || dashArr.indexOf("_") === -1) {
     isGameOver = true
   } 
-  if(dashArr.indexOf("_") === -1) {
-    isGameOver = true
-  }
-  //clean up this
 }
+
+////Notes
 //get a random word
 //break word into single letter strings
-
 //Guess ===
 //put guess in a input 
 ////true if player gets right letter
